7d9bb3714924555009829d00f73db95ff6d3234d
Slava Bozhko <slava.bozhko87@gmail.com>
2021-01-26 23:14:12 +0200

    Fix animation and paddin swiper

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,24 @@ function closeMenu(){
 
 
 
-var isInViewport = function (elem) {
-    var bounding = elem.getBoundingClientRect();
-    return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+let isInViewport = function (elem) {
+// let bounding = elem.getBoundingClientRect();
+//     return (
+//         bounding.top >= 0 &&
+//         bounding.left >= 0 &&
+//         bounding.top + bounding.height / 2 <= (window.innerHeight || document.documentElement.clientHeight)
+//          &&
+//         bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+const rect = elem.getBoundingClientRect();
+    // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+
+    return (vertInView && horInView);
 };
 
 function checkItems(){
@@ -23,6 +33,7 @@ function checkItems(){
     let items = document.querySelectorAll('.animate');
 
     items.forEach(item=>{
+      
         if (isInViewport(item)){
             let animateMode = item.getAttribute('data-animate');
 
