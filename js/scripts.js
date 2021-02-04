426c370c55ad5e61d77c65bfa99eca9b88a7701d
Slava Bozhko <slava.bozhko87@gmail.com>
2021-02-04 20:19:55 +0200

    jq

--- js/scripts.js
+++ js/scripts.js
@@ -190,3 +190,30 @@ const myModal = new HystModal({
     console.log(modal); //modal window object
   },
 });
+
+
+
+//config vars
+const lagAmount = 50;
+const maxSpeed = 100;
+const frameRate = 20;
+const selector = '.pin';
+//code
+let scrollTop = 0;
+let pinTop = 0;
+let lastTime;
+const updatePinPosition = (time) => {
+	if (!lastTime)
+		lastTime = time;
+	let delta = time - lastTime;
+	if (delta >= frameRate){
+		scrollTop = $(window).scrollTop();
+		var move = (scrollTop - pinTop) * delta / (lagAmount + delta);
+		var direction = move === 0 ? 0 : move / Math.abs(move);
+		pinTop = pinTop + Math.min( Math.abs(move), maxSpeed ) * direction;
+		$(selector).css('transform', `translateY(${-move}px`)
+		lastTime = time;
+	}
+	requestAnimationFrame(updatePinPosition);
+}
+requestAnimationFrame(updatePinPosition);
